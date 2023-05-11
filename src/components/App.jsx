3254b28572131a6f503ed3a9089c6c8ec7a975c4
etopritika <dimaprytyka@gmail.com>
2023-05-11 12:09:26 +0300

    init

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,10 +5,11 @@ import Contacts from './Contacts';
 
 export class App extends Component {
   state = {
-    contacts: [{id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    ],
     filter: '',
   };
 
@@ -32,9 +33,11 @@ export class App extends Component {
   };
 
   handleDeleteButton = id => {
-    const updatedContacts = this.state.contacts.filter(contact => contact.id !== id);
+    const updatedContacts = this.state.contacts.filter(
+      contact => contact.id !== id
+    );
     this.setState({ contacts: updatedContacts });
-  }
+  };
 
   render() {
     const itemsToRender = this.state.filter
@@ -43,13 +46,20 @@ export class App extends Component {
     return (
       <section>
         <h1>Phonebook</h1>
-        <ContactForm button="Add contact" onSubmit={this.formSubmitHandler} contacts={this.state.contacts}/>
+        <ContactForm
+          button="Add contact"
+          onSubmit={this.formSubmitHandler}
+          contacts={this.state.contacts}
+        />
         <h2>Contacts</h2>
         <Filter
           inputValue={this.state.filter}
           onChange={this.filterInputChange}
         />
-        <Contacts contacts={itemsToRender} deleteButton={this.handleDeleteButton}/>
+        <Contacts
+          contacts={itemsToRender}
+          deleteButton={this.handleDeleteButton}
+        />
       </section>
     );
   }
